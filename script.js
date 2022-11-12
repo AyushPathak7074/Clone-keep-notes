694431241dfb9f1b2c8459905675690ae88bea0b
Ayush <ayush21xxx013@akgec.ac.in>
2022-11-12 13:14:56 +0530

    changes added

--- script.js
+++ script.js
@@ -1,106 +1,80 @@
+const addButton = document.querySelector("#add");
 
-window.onload = loadTasks;
-
-document.querySelector("form").addEventListener("submit", e => {
-  e.preventDefault();
-  addTask();
-});
-
-function loadTasks() {
-
-  if (localStorage.getItem("tasks") == null) return;
-
-
-  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-
- 
-  tasks.forEach(task => {
-    const list = document.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
-          <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-          <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
-    list.insertBefore(li, list.children[0]);
+const updateLSData = () => {
+  const textAreaData = document.querySelectorAll("textarea");
+  const notes = [];
+  console.log(textAreaData);
+  textAreaData.forEach((note) => {
+    return notes.push(note.value);
   });
-}
+  console.log(notes);
 
-function addTask() {
-  const task = document.querySelector("form input");
-  const list = document.querySelector("ul");
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
 
-  if (task.value === "") {
-    alert("Please add some task!");
-    return false;
-  }
+// <!-- SUbSCRIBE THAPA TECHNICAL YOUTUBE CHANNEL https://www.youtube.com/thapatechnical -->
 
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
 
- 
-  localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
+  const htmlData = `
+    <div class="operation">
+        <button class="edit"> <i class="fas fa-edit"></i> </button>
+        <button class="delete"> <i class="fas fa-trash-alt"></i> </button>
+    </div>
 
-  
-  const li = document.createElement("li");
-  li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
-      <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-      <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
-  list.insertBefore(li, list.children[0]);
- 
-  task.value = "";
-}
+    <div class="main ${text ? "" : "hidden"} "> </div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>  `;
 
-function taskComplete(event) {
-  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-  tasks.forEach(task => {
-    if (task.task === event.nextElementSibling.value) {
-      task.completed = !task.completed;
-    }
-  });
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  event.nextElementSibling.classList.toggle("completed");
-}
+  note.insertAdjacentHTML("afterbegin", htmlData);
+  // console.log(note);
 
-function removeTask(event) {
-  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-  tasks.forEach(task => {
-    if (task.task === event.parentNode.children[1].value) {
-    
-      tasks.splice(tasks.indexOf(task), 1);
-    }
-  });
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  event.parentElement.remove();
-}
+  // getting the References
+  const editButton = note.querySelector(".edit");
+  const delButton = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
 
-
-var currentTask = null;
-
-
-function getCurrentTask(event) {
-  currentTask = event.value;
-}
-
-
-function editTask(event) {
-  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-
-  if (event.value === "") {
-    alert("Task is empty!");
-    event.value = currentTask;
-    return;
-  }
-
-  tasks.forEach(task => {
-    if (task.task === event.value) {
-      alert("Task already exist!");
-      event.value = currentTask;
-      return;
-    }
+  // deleting the node
+  delButton.addEventListener("click", () => {
+    note.remove();
+    updateLSData();
   });
 
-  tasks.forEach(task => {
-    if (task.task === currentTask) {
-      task.task = event.value;
-    }
+  // toggle using edit button
+  textArea.value = text;
+  mainDiv.innerHTML = text;
+
+  editButton.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
   });
-  
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  textArea.addEventListener("change", (event) => {
+    const value = event.target.value;
+    mainDiv.innerHTML = value;
+
+    updateLSData();
+  });
+
+  document.body.appendChild(note);
+  // it appeds a node as the last child of  a node
+};
+
+// getting data back from localStorage
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
 }
+
+addButton.addEventListener("click", () => addNewNote());
+
+
+
+
+
+
+
+
